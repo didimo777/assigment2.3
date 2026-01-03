284e230c8e07fd0a7b067be30d3031125ebaecfb
didimo777 <momenzanovadilnaz@gmail.com>
2026-01-04 03:08:17 +0500

    Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        new FleetApp().run();
+public class Car extends Vehicle implements Servicable {
+
+    public Car(String model, int year, double basePrice) {
+        super(model, year, basePrice);
+    }
+
+    @Override
+    public double calculateInsuranceFee() {
+        return basePrice * 0.05;
+    }
+
+    @Override
+    public void performService() {
+        System.out.println("Car " + model + " serviced");
     }
 }
